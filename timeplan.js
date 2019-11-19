772844f48cb04b2421c1d1bb566e5db0ecaead1e
zweistein83 <53149865+zweistein83@users.noreply.github.com>
2019-11-19 21:08:31 +0100

    uploaded to github

---
+++ timeplan.js
@@ -0,0 +1,220 @@
+//Test
+
+var first_hour = 7;
+var row_height = 5;
+var number_of_headers = 1;
+var days_arr = ["day_1", "day_2", "day_3", "day_4", "day_5", "day_6", "day_7"];
+
+var newJSON = `{
+    "settings": {
+        "day_names": ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "L\u00F8rdag", "S\u00F8ndag"],
+		"time_label": "tid"
+    },
+    "W_45_2019": {
+        "day_1": {"16e84eb4e550002":{"event_name" : "Matematikk", "event_color" : "bgblue", "event_start" : [11,0], "event_end" : [13, 30], "event_location" : "Rom_1"}}
+            
+        ,
+        "day_2": {},
+        "day_3": {},
+        "day_4": {},
+        "day_5": {"16e84eb4e550001":{"event_name" : "Norsk", "event_color" : "bgred", "event_start" : [8,15], "event_end" : [13, 30], "event_location" : "Rom_2"}},
+        "day_6": {},
+        "day_7": {}
+    }
+}
+`
+
+// TODO:
+/*
+	- location / room in event	
+	- info in event
+	-
+*/
+
+//var parsedJSON = JSON.parse(newJSON);
+var myStorage = window.localStorage;
+var parsedJSON = JSON.parse(myStorage.getItem("schedule"));
+var day_names = parsedJSON["settings"]["day_names"]
+var time_label = parsedJSON["settings"]["time_label"]
+document.getElementById("time_label").innerHTML = time_label;
+
+
+function setDayNames() {
+    for (let d = 0; d < 7; d++) {
+        document.getElementById("dayName_" + (d + 1)).innerHTML = day_names[d];
+    }
+}
+setDayNames();
+
+// Takes an integer input and converts to string and adds zero padding
+function str_pad2(input_int){
+	return input_int.toString().padStart(2, "0")
+}
+
+// Formats a an array of hour and minutes to format "HH:MM"
+function format_time(time_arr){
+	let hour = time_arr[0];
+	let min = time_arr[1];
+	return `${str_pad2(hour)}:${str_pad2(min)}`
+}
+
+/*
+	Creates a div containing buttons for editing timetable events.
+*/
+function add_event_buttons(){
+	let new_node = document.createElement("DIV");
+	new_node.classList.add("event_buttons_container");
+	let delete_btn = '<input type="button" class="event_button" value="x" onclick="delete_event_btn(this)">';
+	let other_btn = '<input type="button" class="event_button" value="y" onclick="delete_event_btn(this)">';	
+	new_node.innerHTML = delete_btn + other_btn;
+	return new_node;
+}
+
+// Creates a div formed like the example below:
+// example <div class = "event bgblue" style ="top: 15%; height: 10%;">Subject 1<br>09:00 - 11:00</div>
+function draw_timetable_events(event_week, event_id, event_name, event_day, event_start, event_end, event_location, color_class) {
+    
+	const hourMinToHour = (h,m)=>(h + (m / 60)); // converts from hours and minutes to hours,.
+	const createSpan = (txt, s_class) => ('<span class = ' + s_class + '>' + txt + '</span>');
+
+	let event_start_hours = hourMinToHour(event_start[0], event_start[1]);
+	let event_end_hours = 	hourMinToHour(event_end[0], event_end[1]);
+	let event_duration_hours = event_end_hours - event_start_hours;
+	
+	let new_node = document.createElement("DIV");
+    new_node.classList.add(color_class);
+    new_node.classList.add("event");
+    new_node.style.top = (((event_start_hours
+						+ number_of_headers)
+						- first_hour) * row_height) + "%"
+    // event box height: start of coordinate system is 1 * row_height. Each hour is row_height %
+    new_node.style.height = event_duration_hours * row_height + "%"
+	new_node.id = event_id;
+	new_node.setAttribute("event_week", event_week);
+    
+	let event_start_formatted = format_time(event_start); // HH:MM
+	let event_end_formatted = 	format_time(event_end);	 // HH:MM
+	
+	let event_name_HTML = createSpan(event_name, "event_name");
+	let event_start_end_HTML = createSpan(event_start_formatted + "-" + event_end_formatted, "event_time");
+	let event_location_HTML = createSpan(event_location, "event_location");
+	
+	// Add event_name, start and end times to the div new_node.	
+	new_node.innerHTML = (event_name_HTML + "<br>"
+						+ event_start_end_HTML + "<br>"						
+						+ event_location_HTML);
+	
+	// Appends the div containing the event to the div containing the specified day.
+	new_node.appendChild(add_event_buttons());
+    document.getElementById(event_day).appendChild(new_node);
+}
+
+
+
+function buttonTest(el){
+	console.log(el);
+	console.log(el.parentNode.parentNode.id);
+	let par_id = el.parentNode.parentNode.id;
+	console.log(JSON.parse(par_id));
+}
+
+// TODO: Remove event from "database"
+function delete_event_btn(evt){	
+	btn_parent_node = evt.parentNode.parentNode; // the delete button is inside a child of the event. <div EVENT><div>BUTTON</div></div>
+	
+	// Deletes event from database
+	let event_id = btn_parent_node.id;
+	let event_day = btn_parent_node.parentNode.id;
+	let event_week = btn_parent_node.getAttribute("event_week");
+	delete parsedJSON[event_week][event_day][event_id];
+
+	btn_parent_node.parentNode.removeChild(btn_parent_node);
+}
+
+function clear_element_by_class(class_name){	
+	let elems = document.getElementsByClassName(class_name);	
+	while (elems.length > 0){
+		elems[0].parentNode.removeChild(elems[0]);
+	}
+	
+}
+
+function clear_all_events(){
+	clear_element_by_class("event");
+}
+
+function get_unique_identifier(){
+	const to_hex = n => n.toString(16);
+	let time_stamp = Date.now();
+	time_stamp = to_hex(time_stamp);
+	let random_suffix = Math.floor(Math.random()*1000);
+	random_suffix = to_hex(random_suffix).padStart(4,"0");
+	return time_stamp+random_suffix;
+}
+	
+
+
+/*
+	week_id: W_WW_YYYY (WW = week number).
+*/
+function read_events_week(week_id){
+	var selected_week = parsedJSON[week_id];
+
+
+	//console.log(selected_week);
+
+	for (let day of days_arr) {
+		//console.log(day);
+		let current_day = selected_week[day];
+		//console.log(current_day);
+		
+		for (let evt_id in current_day) {
+			let evt = current_day[evt_id];
+			draw_timetable_events(week_id,
+								evt_id,
+								evt["event_name"], 
+								day, 
+								evt["event_start"], 
+								evt["event_end"],
+								evt["event_location"], 
+								evt["event_color"])
+		}	
+	}
+}
+
+function create_event(event_week_id, event_name, event_day, event_start, event_end, event_location, event_color){
+	parsedJSON[event_week_id][event_day][get_unique_identifier()] = {"event_name": event_name, "event_start": event_start,"event_end": event_end, "event_location": event_location, "event_color": event_color};
+}
+
+function save_events_to_local (){
+	myStorage.setItem("schedule", JSON.stringify(parsedJSON));
+}
+
+/*
+	week_id: W_WW_YYYY
+*/
+function delete_week(week_id){
+	delete parsedJSON[week_id];
+	console.log("week "+ week_id + "deleted");
+}
+
+function create_week(week_id){
+	parsedJSON[week_id] = {
+        "day_1": {},
+        "day_2": {},
+        "day_3": {},
+        "day_4": {},
+        "day_5": {},
+        "day_6": {},
+        "day_7": {}}
+}
+
+function test_data(){
+	create_event("W_45_2019", "Test", "day_6",[10,25],[12,55], "rom_x", "bgblue");
+	create_event("W_45_2019", "Test2", "day_2",[8,30],[19,01], "rom_y", "bgred");
+}
+
+
+
+read_events_week("W_45_2019");
+//add_timetable_event("W_45_2019","1021201","Matematikk", "day_2", [9, 0], [10, 0], "Rom_3", "bgred");
---
+++ timeplan_event.js
@@ -0,0 +1,3 @@
+class timetable_event{
+    
+}
